8db787499f4ddc68c842bb5e1ef3f9e05f274ead
KirraDark <blackbloodyring@gmail.com>
2023-11-13 18:45:32 +0300

    сделать джинджу

--- django_my/flowers/models.py
+++ django_my/flowers/models.py
@@ -7,6 +7,19 @@ class Articles(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=True)
+    category = models.CharField("Категория", max_length=50, blank=True, null=True)
+
+    def __str__(self):
+        return f"Название: {self.name}"
+
+class orchids(models.Model):
+    name = models.CharField("Название", max_length=55)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    img = models.ImageField(upload_to='media/')
+    time_create = models.DateTimeField(auto_now_add=True)
+    time_update = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=True)
+    category = models.CharField("Категория", max_length=50, blank=True, null=True)
 
     def __str__(self):
         return f"Название: {self.name}"
--- django_my/flowers/urls.py
+++ django_my/flowers/urls.py
@@ -5,6 +5,9 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('pions/', views.ArticlesClass, name='pion')
+    path('pions/', views.ArticlesClass, name='pion'),
+    path('orchids/', views.orchids_view, name='orchids'),
+    path('pions/<int:article_id>/', views.article_detail, name='article_detail'),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
